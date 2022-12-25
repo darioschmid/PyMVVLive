bcf53a8a08b69052c5fbf6e1f23f4b04ab23a7e7
Dario Schmid <darioschmid99@outlook.com>
2022-12-25 18:30:29 +0100

    sd

--- tests/test_punctuality.py
+++
@@ -1,8 +0,0 @@
-from mvv_sbahn import punctuality
-import re
-
-
-def test_punctuality():
-    for line in ["S1", "S2", "S3", "S4", "S6", "S7", "S8"]:
-        assert int(punctuality.punctuality(line)) in range(0, 101)
-    assert punctuality.punctuality("S20") == "-" or int(punctuality.punctuality("S20")) in range(0, 101)
