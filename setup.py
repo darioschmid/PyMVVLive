75fe4b6a9d01cfc2b2f3c71ed0e6f3762659f7ee
Dario Schmid <darioschmid99@outlook.com>
2022-12-26 16:21:23 +0100

    updated github link

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
-    url='https://github.com/darioschmid/MVVLive',
+    url='https://github.com/darioschmid/PyMVVLive',
 )
