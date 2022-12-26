b13be15e4c0456c7dd3dc6649b83ba335e88402c
Dario Schmid <darioschmid99@outlook.com>
2022-12-26 15:41:31 +0100

    updated soupsieve version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Dario Schmid",
     author_email="darioschmid99@outlook.com",
     license="?",
-    install_requires=['soupsieve==2.3.2', 'beautifulsoup4==4.11.1', 'requests'],
+    install_requires=['soupsieve==2.3.2.post1', 'beautifulsoup4==4.11.1', 'requests'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
