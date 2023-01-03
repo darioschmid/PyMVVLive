1a95596fe2383fc4887fb34c254cd7939029e0d5
Dario Schmid <darioschmid99@outlook.com>
2023-01-03 23:53:14 +0100

    moved

--- test.py
+++ test.py
@@ -7,32 +7,44 @@ line = "S3"
 
 def test(live):
     try:
+        live.update_punctuality()
         print(live.punctuality)
     except:
         print("No punctuality data available")
 
     try:
+        live.update_data()
         data = json.dumps(live.data, indent=4, ensure_ascii=False)
+        print("data received.")
         #print(data)
     except:
         print("No data available")
 
     try:
-        departures = json.dumps(live.departures, indent=4, ensure_ascii=False)
-        #print(departures)
-    except:
-        print("No departures available")
-
-    try:
+        live.update_serving_lines()
         serving_lines = json.dumps(live.serving_lines, indent=4, ensure_ascii=False)
+        print("serving_lines received.")
         #print(serving_lines)
     except:
         print("No serving lines available")
 
+    try:
+        live.update_departures()
+        departures = json.dumps(live.departures, indent=4, ensure_ascii=False)
+        print("departures received.")
+        #print(departures)
+    except:
+        print("No departures available")
+
 if __name__ == "__main__":
     live = MVVLive.MVVLive(stop_name=stop_name)
+    print("Testing stop_name...")
     test(live)
+
     live = MVVLive.MVVLive(stop_id=stop_id)
+    print("\nTesting stop_id...")
     test(live)
+
     live = MVVLive.MVVLive(line=line)
+    print("\nTesting line...")
     test(live)
